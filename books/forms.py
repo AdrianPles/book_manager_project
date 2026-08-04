cb64612515ee0a1b46b40112efd24b0a94f6b41b
AdrianPles <adi_kite@yahoo.com>
2026-08-04 20:55:31 +0300

    added book image deletion

--- books/forms.py
+++ books/forms.py
@@ -5,4 +5,4 @@ from .models import Book
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
-        fields = ["title", "author"]
+        fields = ["title", "author", "cover_image"]
